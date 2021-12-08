37161f5fe25389a209e8d77cc07de48f2afb7dca
kelsey-schenk <kelsey.schenk@att.net>
2021-12-07 19:02:35 -0500

    inquirer

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-// Array method .slice() returns a brand-new array based on process.argv starting at the third index and ending
-// with the final index
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+// if (err) throw err;
 
-
-
-// Lack of parentheses around the 'profileDataArr' parameter
-const printProfileData = (profileDataArr) => {
-    // This ...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
+// console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
     }
-
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem)); 
-
-
-printProfileData(profileDataArgs);}
+])
+.then(answers => console.log(answers));
